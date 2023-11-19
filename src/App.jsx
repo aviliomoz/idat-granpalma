bed292f3a953012b4aeb155fe29ec29a5c7e127d
Avilio Muñoz <aviliomuoz@gmail.com>
2023-11-19 18:00:20 -0500

    hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { MainLayout } from "./layouts/main-layout";
 import { HomePage } from "./pages/home-page";
