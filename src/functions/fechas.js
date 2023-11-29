32333ddf3f6907efe8ee13060259718580016d1d
Avilio Muñoz <aviliomuoz@gmail.com>
2023-11-28 19:16:21 -0500

    date formatting

--- src/functions/fechas.js
+++ src/functions/fechas.js
@@ -1,9 +1,14 @@
+import dayjs from "dayjs";
+
 export const obtenerFecha = (suma = 0) => {
-    const hoy = new Date();
+    // const hoy = new Date();
   
-    const a = hoy.getFullYear();
-    const m = (hoy.getMonth() + 1).toString().padStart(2, "0");
-    const d = (hoy.getDate() + suma).toString().padStart(2, "0");
+    // const a = hoy.getFullYear();
+    // const m = (hoy.getMonth() + 1).toString().padStart(2, "0");
+    // const d = (hoy.getDate() + suma).toString().padStart(2, "0");
   
-    return `${a}-${m}-${d}`;
+    // return `${a}-${m}-${d}`;
+
+    return dayjs().add(suma, "days").format("YYYY-MM-DD");
+
   };
